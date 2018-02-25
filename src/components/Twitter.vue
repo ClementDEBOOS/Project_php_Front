2315fd26003b5d1554c4c5115546aa9c04755697
DEBOOS <clement.deboos@gmail.com>
2018-02-25 17:51:49 +0100

    Twitter correct

--- src/components/Twitter.vue
+++ src/components/Twitter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div></div>
+    <div><Timeline :id="'twitterdev'" :sourceType="'profile'" :options="{ tweetLimit: '3' }"/></div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Tiwtter from '@/components/Twitter'
+import Twitter from '@/components/Twitter'
 
 Vue.use(Router)
 
