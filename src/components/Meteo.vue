38a3a9c68a87404f7a692dde91033e802105056c
beryl <ebdesign29@gmail.com>
2018-02-25 17:43:54 +0100

    Composant API Météo

--- src/components/Meteo.vue
+++ src/components/Meteo.vue
@@ -1,10 +1,36 @@
 <template>
  <v-container id="meteo" grid-list-md>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <h1>AUJOURD'HUI</h1>
-        <h2>BREST</h2>
-      </v-flex>
+    <v-layout column>
+      <v-layout row wrap>
+        <v-flex xs4>
+          <h1>AUJOURD'HUI</h1>
+          <h2>BREST</h2>
+        </v-flex>
+        <v-flex xs4 text-xs-center>
+          <p class="temperature">{{day0Temperature}}°</p>
+        </v-flex>
+        <v-flex xs4>
+          <h1>AUJOURD'HUI</h1>
+          <h2>BREST</h2>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs4 v-for="i in 3" :key="`4${i}`">
+          <v-card>
+            <v-layout row wrap>
+              <v-flex xs4>
+              LUNDI
+              </v-flex>
+              <v-flex xs4>
+              {{day+i+Temperature}}°
+              </v-flex>
+              <v-flex xs4>
+              LUNDI
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-layout>
   </v-container>
 </template>
@@ -14,7 +40,10 @@ export default {
   name: 'Meteo',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      day0Temperature: 8,
+      day1Temperature: 8,
+      day2Temperature: 8,
+      day3Temperature: 8
     }
   }
 }
@@ -26,4 +55,11 @@ export default {
 #meteo{
   background-color:#0078d7;
 }
+
+.card{
+  background-color: rgba(0, 0, 0, 0.5);
+  padding:8px;
+  color:white;
+}
+
 </style>
