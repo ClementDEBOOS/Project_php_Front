b65256b02dcbccfc348b87914dfd493976a48ed8
beryl <ebdesign29@gmail.com>
2018-02-25 16:00:15 +0100

    Couleurs de la charte graphique

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,23 @@ export default {
 </script>
 
 <style>
+
+.background-blue1{
+  background-color:#0078d7;
+}
+
+.background-blue2{
+  background-color:#0099bc;
+}
+
+.background-red{
+  background-color:#e74856;
+}
+
+.background-yellow{
+  background-color:#ffb900;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +36,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2, h3 {
+  font-weight: normal;
+  color:white;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
 }
 </style>
