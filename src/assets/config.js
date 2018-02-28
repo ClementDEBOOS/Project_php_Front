aa0945ba841f0edd97a7c52b46ee837c071d257c
Unknown <clement.deboos@gmail.com>
2018-02-28 20:48:52 +0100

    Factory config

---
+++ src/assets/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'DPDeYRZeL0cRori83wvIYQ3JT',
+  consumer_secret: '2r89PIvw8fokkD5uFWbn7BCYPc8UF1flOYrkoxizWQfULWhQ17',
+  access_token: '2745742222-0TtPDEt4eSYGz3tMbWtaxScm81emgZclkxKyJIU',
+  access_token_secret: 'SVxKbWIenZKXvty7tXRj6KMX3hHt0UgHM7F2VYpEUbHkW'
+}
--- src/components/Twitter.vue
+++ src/components/Twitter.vue
@@ -24,7 +24,7 @@
 /* eslint-disable handle-callback-err */
 
 export default {
-  name: 'twitter',
+  name: 'Twitter',
   data () {
     return {
 
@@ -35,24 +35,18 @@ export default {
     getTweets () {
       var Twit = require('twit')
 
-      var T = new Twit({
-        consumer_key: 'DPDeYRZeL0cRori83wvIYQ3JT',
-        consumer_secret: '2r89PIvw8fokkD5uFWbn7BCYPc8UF1flOYrkoxizWQfULWhQ17',
-        access_token: '2745742222-0TtPDEt4eSYGz3tMbWtaxScm81emgZclkxKyJIU',
-        access_token_secret: 'SVxKbWIenZKXvty7tXRj6KMX3hHt0UgHM7F2VYpEUbHkW',
-        timeout_ms: 60 * 1000
-      })
+      var config = require('../assets/config')
+      var T = new Twit(config)
 
       var params = {
         q: 'rainbow',
         count: 2
       }
 
-      T.get('search/tweets', params, gotData()/* ).then(response => {
+      T.get('search/tweets', params, gotData/* ).then(response => {
         this.tweets = response
         console.log(response)
       } */)
-
       function gotData (err, data, response) {
         console.log(data)
       }
