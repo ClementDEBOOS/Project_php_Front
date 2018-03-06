28a8e9c6df225fe889dc96fc858b9655916c5bf8
beryl <ebdesign29@gmail.com>
2018-03-06 17:57:53 +0100

    Modifications des images

---
+++ src/components/Planning.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="planning">
+    <img src="static/img/capture.PNG" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Planning'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
