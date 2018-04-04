4cff73fd9bc8dad8bfaaf95a13eb8da616570485
DEBOOS <clement.deboos@gmail.com>
2018-04-04 16:03:59 +0200

    Main structure of planning page

--- src/components/Planning.vue
+++ src/components/Planning.vue
@@ -1,55 +1,35 @@
 <template>
-  <div class="padding-5">
-    <div id="planning">
+  <div id="planning">
+    <div class="padding-5">
       <v-app>
-        <v-container grid-list-xl text-xl-center class="planning-content ">
+        <v-container grid-list-xl text-xl-center class="planning-content">
           <v-layout row wrap>
-            <v-flex xs20 class="padding-5" align="center">
-              <v-card dark color="purple">
+            <v-flex offset-xs1 xs10 class="padding-5">
+              <v-card class="edit_title">
                 <v-card-text>Planning</v-card-text>
               </v-card>
             </v-flex>
-          </v-layout>
-          <v-layout row wrap>
-            <v-flex offset-xs1 class="padding-5" v-for="n in 3" :key="n">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-left">RIL 2016</v-card-text>
-              </v-card>
+          </v-layout><br><br>
+          <v-layout>
+            <v-flex v-for="n in 3" :key="n">
+              <v-layout row wrap>
+                <v-flex xs10 offset-xs1 class="padding-5">
+                    <v-card-text class="class_text text-md-left" >RIL 2016</v-card-text>
+                </v-flex>
+              </v-layout>
+              <v-layout  row wrap v-for="n in 7" :key="n">
+                <v-flex offset-xs1 xs3 class="padding-5 line_border">
+                  <v-card class="color_days">
+                    <v-card-text class="px-20 text-md-center">LUNDI</v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs7 class="padding-5">
+                  <v-card class="color_classroom">
+                    <v-card-text class="px-20 text-md-center">Les veilles charrues</v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
             </v-flex>
-          </v-layout>
-          <v-layout row wrap  v-for="n in 7" :key="n">
-
-            <v-flex offset-xs1 xs1 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">LUNDI</v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs2 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">Les veilles charrues</v-card-text>
-              </v-card>
-            </v-flex>
-<!--fluid grid-list
-            <v-flex xs1 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">LUNDI</v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs3 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">Les veilles charrues</v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs1 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">LUNDI</v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs3 class="padding-5">
-              <v-card dark color="primary">
-                <v-card-text class="px-20 text-md-center">Les veilles charrues</v-card-text>
-              </v-card>
-            </v-flex>-->
           </v-layout>
         </v-container>
       </v-app>
@@ -83,8 +63,7 @@ export default {
 
 <style scoped>
   #planning{
-    max-height: 910px;
-    height:910px;
+    height: 100%;
   }
 
   .planning-content{
@@ -92,11 +71,34 @@ export default {
     height: 100%;
     background: white;
   }
-
-  .planning-content img{
-    height:100%;
-  }
   .padding-5{
     padding: 5px !important;
+  }
+
+  .class_text{
+    font-size: 26px;
+    color: black;
+    font-weight: bold;
+  }
+  .color_classroom{
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+    background: #3FC380;
+  }
+  .color_days{
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+    background: #6C7A89;
+  }
+  .line_border{
+    border-left: #3FC380 3px solid;
+  }
+  .edit_title{
+    font-size: 30px;
+    color: white;
+    font-weight: bold;
+    background: #2574A9;
   }
 </style>
